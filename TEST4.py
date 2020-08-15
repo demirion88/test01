3e8397a93bda4cf27ac362f535e0a3fde195dd78
blackpigeon <61138426+demirion88@users.noreply.github.com>
2020-08-15 16:53:47 +0900

    Revert "NEW FILE 4"
    This reverts commit 3fc4134ecc7e85f74482b7051d277976e86396b8.

--- TEST4.py
+++ TEST4.py
@@ -1,9 +1,36 @@
+#OH MY GOD
 
-a={
-    'name':'pey',
-    'phone':'011213123123',
-    'birth':'1118'
 
-}
-print(a.items())
-print(a['ovv'])=4
+# print(pd.__version__)
+
+# sr = pd.Series([17000,18000,1000,5000],index=["피자","치킨","콜라","맥주"])
+# print(sr)
+# print(sr.values)
+# print(sr.index)
+
+# data = [
+#     ['1000','steve',90.72],
+#     ['1001','james', 78.09],
+#     ['1002', 'Doyeon', 98.43],
+#     ['1003', 'Jane', 64.19],
+#     ['1004', 'Pilwoong', 81.30],
+#     ['1005', 'Tony', 99.14]
+# ]
+
+
+
+
+#df = pd.DataFrame(data,columns=['학번','이름','점수'])
+
+# data = { '학번' : ['1000', '1001', '1002', '1003', '1004', '1005'],
+# '이름' : [ 'Steve', 'James', 'Doyeon', 'Jane', 'Pilwoong', 'Tony'],
+#          '점수': [90.72, 78.09, 98.43, 64.19, 81.30, 99.14]}
+# df = pd.DataFrame(data)
+
+import pandas as pd
+import pandas_profiling
+
+data=pd.read_csv(r'D:\ProgramData\Dataset\spam.csv',encoding='latin1')
+
+pr=data.profile_report()
+pr.to_file('./pr_report.html') # pr_report.html 파일로 저장
--- notebook.py
+++
@@ -1,7 +0,0 @@
-v=list(range(10,20))
-
-
-print([i for i in v if i==12])
-
-
-
